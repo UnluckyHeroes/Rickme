540162d30b63175e8e40cbf20afa6075945151a5
lucho1 <luchosuaya@hotmail.com>
2018-06-19 20:07:42 +0200

    Updated Modules for UI
    -Added a new module: Module UI
    
    -Deleted ModuleUserInterface & ModuleFont (Are included in ModuleUI)
    
    -Progressed with ModuleUI header: Done things related to Fonts and some parameters, commented things on Ranking
    
    -Added a header, "Comments on UI" in order to put there comments related to UI of previous modules for UI and fonts so I could add them to the new module

---
+++ Rickme/ModuleUI.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "ModuleUI.h"
+#include "Globals.h"
+#include "Application.h"
+
+#include <iostream>
+#include <string.h>
+#include <cstdio>
+
+ModuleUI::ModuleUI()
+{
+}
+
+
+ModuleUI::~ModuleUI()
+{
+}
